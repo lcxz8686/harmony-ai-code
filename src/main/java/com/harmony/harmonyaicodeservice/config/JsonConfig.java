af2d3b2812519c724edeb7a959fa64c9960ce86a
harmony <1585342785@qq.com>
2026-01-29 01:36:56 +0800

    fix: 精度丢失问题修复

---
+++ src/main/java/com/harmony/harmonyaicodeservice/config/JsonConfig.java
@@ -0,0 +1,30 @@
+package com.harmony.harmonyaicodeservice.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+/**
+ * Spring MVC Json 配置。
+ * 针对 Long 类型数据转换为 JSON 时的精度丢失问题
+ */
+@JsonComponent
+public class JsonConfig {
+
+    /**
+     * 添加 Long 转 json 精度丢失的配置
+     */
+    @Bean
+    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(Long.class, ToStringSerializer.instance);
+        module.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        objectMapper.registerModule(module);
+        return objectMapper;
+    }
+}
+
